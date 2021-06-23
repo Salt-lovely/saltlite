708aa20607d00c8f86163c7382f48d77d53747cd
Salt-lovely <732237136@qq.com>
2021-06-24 01:15:58 +0800

    FIX: 副作用函数提示

--- src/typings/safe.d.ts
+++ src/typings/safe.d.ts
@@ -4,10 +4,16 @@ interface SaltSafe {
   isUnsafePropName(propName: string | number): boolean;
   /** 判断这个属性名是否为安全属性名 */
   isSafePropName(propName: string | number): boolean;
-  /** 将可疑的属性名置为`undefined` */
+  /**
+   * 将可疑的属性名置为`undefined`
+   *
+   * **副作用函数**
+   */
   filterUnsafeProp<T extends object>(obj: T): T;
   /**
    * 遍历数组中的属性，跳过`undefined`和可疑属性
+   *
+   * **副作用函数**
    * @param obj 存在可疑属性对象
    * @param fn 每个循环体调用的回调函数
    * @param deleteUnsafeProp 是否删除可疑属性（置为`undefined`）默认不删除
