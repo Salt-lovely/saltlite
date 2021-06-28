b213b0b3b61312adbd7f0700ae6de39f38f09ae5
Salt-lovely <732237136@qq.com>
2021-06-28 12:48:28 +0800

    CHANGE: 更新许可证到木兰宽松许可证2.0

--- src/entry.ts
+++ src/entry.ts
@@ -3,6 +3,17 @@
  * @License MulanPSL
  * @Description Salt lovely's light-weight frontend lib
  */
+/*!
+Copyright (c) 2021 Salt-lovely
+Saltlite is licensed under Mulan PSL v2.
+You can use this software according to the terms and conditions of the Mulan PSL v2.
+You may obtain a copy of Mulan PSL v2 at:
+         http://license.coscl.org.cn/MulanPSL2
+THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+See the Mulan PSL v2 for more details.
+*/
 // 整个SaltLite的入口
 import polyfillAndMore from './polyfill/index';
 import safe from './safe/index';
